aec6135350eee72cb8a8a20a39f5c5459019a78a
Edward Bichetero <ebichete@gmail.com>
2017-12-07 12:49:33 +0300

    Move the cannula app to the root of the project url space

--- rhites_ec_web/urls.py
+++ rhites_ec_web/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^cannula/', include('cannula.urls')),
+    url(r'^$', 'cannula.views.index'),
+    url(r'', include('cannula.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
