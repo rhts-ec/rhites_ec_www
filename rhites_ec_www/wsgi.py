cf3c2b43e50af12264356389fcec51a9f72b7e4d
Assey Alfred M <asseym@gmail.com>
2018-02-09 12:06:21 +0300

    apply all changes from aa

--- rhites_ec_www/wsgi.py
+++ rhites_ec_www/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rhites_ec_web project.
+WSGI config for rhites_ec_www project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rhites_ec_web.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rhites_ec_www.settings")
 
 application = get_wsgi_application()
